c8e0eab8a402f7496651bc772c51827fd9cb40c0
oscarfauredev <143498273+oscarfauredev@users.noreply.github.com>
2024-02-22 16:22:32 +0000

    v.0.2.0 add test if z is in Victor Hugo .txt

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    const char texte[] = "Victor Hugo est un poète, z dramaturge, écrivain, romancier et dessinateur romantique français, né le 26 février 1802 à Besançon et mort le 22 mai 1885 à Paris. Il est considéré comme l'un des plus importants écrivains de langue française.";
+    int testVariable = 0;
+
+    const char *ptr = texte;
+
+    while (*ptr != '\0') {
+        if (*ptr == 'z') {
+            testVariable = 1;
+            break;
+        }
+
+        ptr++;
+    }
+
+    if (testVariable) {
+        printf("Le caractere 'z' est present dans la chaine.\n");
+    } else {
+        printf("Le caractere 'z' n'est pas present dans la chaine.\n");
+    }
+
     return 0;
 }
